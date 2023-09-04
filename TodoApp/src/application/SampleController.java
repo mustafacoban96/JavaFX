0f4f26f57b6a3c11f5b471110d604b097a899377
mustafacoban96 <mustafa.coban@std.hku.edu.tr>
2023-09-04 22:58:29 +0300

    ui components are defined as attribute in SampleController

--- TodoApp/src/application/SampleController.java
+++ TodoApp/src/application/SampleController.java
@@ -1,5 +1,19 @@
 package application;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
+
 public class SampleController {
+	@FXML
+	private Button addMissionBtn;
+	@FXML
+	private TextField missionDescription;
+	@FXML
+	private DatePicker datePicker;
+	@FXML
+	private ListView<String> eventListView;
 	
 }
