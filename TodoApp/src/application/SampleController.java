947367fb83280ff42a903c563ef8d995890e2d9f
mustafacoban96 <mustafa.coban@std.hku.edu.tr>
2023-09-05 14:39:01 +0300

    deserializer integrated in sample

--- TodoApp/src/application/SampleController.java
+++ TodoApp/src/application/SampleController.java
@@ -10,12 +10,15 @@ import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Button;
 import javafx.scene.control.DatePicker;
 import javafx.scene.control.ListView;
 import javafx.scene.control.TextField;
 import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyEvent;
+import missionSerializer.MissionSerializer;
 
 public class SampleController implements Initializable{
 	@FXML
@@ -59,6 +62,15 @@ public class SampleController implements Initializable{
 	public void initialize(URL arg0, ResourceBundle arg1) {
 		
 		datePicker.setValue(LocalDate.now());
+		
+		try {
+			var missions = MissionSerializer.deserializer();
+			eventListView.getItems().addAll(missions);
+		}catch (Exception e) {
+			Alert alert = new Alert(AlertType.ERROR);
+			alert.setTitle("TodoApp Hatası");
+			alert.setHeaderText("Görevler Yüklenmedi dosya sistemini kontrol ediniz.");
+		}
 	}
 	
 }
